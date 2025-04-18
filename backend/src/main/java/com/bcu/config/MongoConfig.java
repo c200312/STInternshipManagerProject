065e798c4cf921b28e68dec10172080fc3dcff33
c200312 <cdurian80@gmail.com>
2025-04-18 09:15:37 +0800

    feat: mongo密码解耦

--- backend/src/main/java/com/bcu/config/MongoConfig.java
+++ backend/src/main/java/com/bcu/config/MongoConfig.java
@@ -1,30 +1,34 @@
 package com.bcu.config;
 
-import com.mongodb.MongoClientSettings;
-import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
-import java.util.concurrent.TimeUnit;
-
 @Configuration
+@PropertySource("classpath:mongo.properties") // 别忘了加 classpath:
 public class MongoConfig {
+
+    @Value("${mongo.host}")
+    private String host;
+
+    @Value("${mongo.port}")
+    private int port;
+
+    @Value("${mongo.database}")
+    private String database;
+
     @Bean
     public MongoClient mongoClient() {
-        MongoClientSettings settings = MongoClientSettings.builder()
-                .applyToSocketSettings(builder -> builder
-                        .connectTimeout(5000, TimeUnit.MILLISECONDS) // 设置连接超时时间为5秒
-                        .readTimeout(5000, TimeUnit.MILLISECONDS)) // 设置读取超时时间为5秒
-                .applyToClusterSettings(builder -> builder
-                        .serverSelectionTimeout(5000, TimeUnit.MILLISECONDS)) // 设置服务器选择超时时间为5秒
-                .build();
-        return MongoClients.create(settings);
+        String connectionString = "mongodb://" + host + ":" + port;
+        return MongoClients.create(connectionString);
     }
 
     @Bean
     public MongoTemplate mongoTemplate() {
-        return new MongoTemplate(mongoClient(), "duser");
+        return new MongoTemplate(mongoClient(), database);
     }
 }
