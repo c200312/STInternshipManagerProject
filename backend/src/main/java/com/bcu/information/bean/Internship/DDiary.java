d34b28b87b430d583f75c29c06e56382aeae3c11
reigadegr <2722688642@qq.com>
2025-04-23 00:49:50 +0800

    chore: lombok简化实体类

--- backend/src/main/java/com/bcu/information/bean/Internship/DDiary.java
+++ backend/src/main/java/com/bcu/information/bean/Internship/DDiary.java
@@ -1,25 +1,12 @@
 package com.bcu.information.bean.Internship;
 
+import lombok.Data;
+
 /*
  * 周学生记实体类
  * */
+@Data
 public class DDiary {
     private Integer week;
     private String content;
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public Integer getWeek() {
-        return week;
-    }
-
-    public void setWeek(Integer week) {
-        this.week = week;
-    }
 }
--- backend/src/main/java/com/bcu/information/bean/Internship/DUser.java
+++ backend/src/main/java/com/bcu/information/bean/Internship/DUser.java
@@ -1,6 +1,8 @@
 package com.bcu.information.bean.Internship;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -8,6 +10,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Document(collection = "users")
 public class DUser {
     @Id
@@ -17,55 +21,4 @@ public class DUser {
     private List<DCompany> company = new ArrayList<>();
     private List<DDiary> diary = new ArrayList<>();
     private List<DComment> comment = new ArrayList<>();
-
-    public DUser() {
-    }
-
-    public DUser(List<DComment> comment, List<DCompany> company, List<DDiary> diary, String id, String name) {
-        this.comment = comment;
-        this.company = company;
-        this.diary = diary;
-        this.id = id;
-        this.name = name;
-    }
-
-    public List<DComment> getComment() {
-        return comment;
-    }
-
-    public void setComment(List<DComment> comment) {
-        this.comment = comment;
-    }
-
-    public List<DCompany> getCompany() {
-        return company;
-    }
-
-    public void setCompany(List<DCompany> company) {
-        this.company = company;
-    }
-
-    public List<DDiary> getDiary() {
-        return diary;
-    }
-
-    public void setDiary(List<DDiary> diary) {
-        this.diary = diary;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
