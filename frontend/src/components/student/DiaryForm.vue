e95b1c45a8003a01c668975de14a145874cbe64a
yichen lv <15757479+lvyic@user.noreply.gitee.com>
2025-06-13 01:12:40 +0000

    update frontend/src/components/student/DiaryForm.vue.
    Signed-off-by: yichen lv <15757479+lvyic@user.noreply.gitee.com>

--- frontend/src/components/student/DiaryForm.vue
+++ frontend/src/components/student/DiaryForm.vue
@@ -5,13 +5,13 @@
       <h4>电子签名</h4>
       <div class="signature-container">
         <el-upload
-          class="signature-uploader"
-          :action="`/api/student/signature/${userName}`"
-          :show-file-list="false"
-          :on-success="handleSignatureSuccess"
-          :on-error="handleSignatureError"
-          :before-upload="beforeSignatureUpload"
-          accept="image/*"
+            class="signature-uploader"
+            :action="`/api/student/signature/${userName}`"
+            :show-file-list="false"
+            :on-success="handleSignatureSuccess"
+            :on-error="handleSignatureError"
+            :before-upload="beforeSignatureUpload"
+            accept="image/*"
         >
           <img v-if="signatureUrl" :src="signatureUrl" class="signature-image" />
           <el-icon v-else class="signature-uploader-icon"><Plus /></el-icon>
@@ -167,18 +167,18 @@
       </el-form-item>
 
       <el-form-item>
-        <el-button 
-          type="primary" 
-          @click="handleSubmit"
-          :disabled="!canEdit()"
+        <el-button
+            type="primary"
+            @click="handleSubmit"
+            :disabled="!canEdit()"
         >
           保存
         </el-button>
-        <el-button 
-          type="success" 
-          @click="submitForReview"
-          :disabled="!canSubmitForReview()"
-          style="margin-left: 10px;"
+        <el-button
+            type="success"
+            @click="submitForReview"
+            :disabled="!canSubmitForReview()"
+            style="margin-left: 10px;"
         >
           提交审核
         </el-button>
@@ -247,10 +247,10 @@ const saveDiary = async () => {
   try {
     if (selectedWeek.value !== 17) {
       await axios.patch(`/duser/${props.userName}`, {
-        diary: [{ 
-          week: selectedWeek.value, 
+        diary: [{
+          week: selectedWeek.value,
           content: content.value,
-          diaryDate: diaryDate.value 
+          diaryDate: diaryDate.value
         }]
       })
     } else {
@@ -364,7 +364,7 @@ const beforeSignatureUpload = (file) => {
 
 // 上传成功处理
 const handleSignatureSuccess = (response) => {
-  if (response.code === 200) {
+  if (response.code === "200") {
     signatureUrl.value = response.data.signatureUrl
     ElMessage.success('电子签名上传成功')
   } else {
